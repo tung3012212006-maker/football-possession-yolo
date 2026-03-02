7a34204da8736030e171d20a30fd81958acba006
Ductung3012 <tung3012212006@gmail.com>
2026-03-02 16:16:43 +0700

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,17 +1,19 @@
 from ultralytics import YOLO
 import cv2
 
-# Load model
+# Load the fine-tuned YOLO model
 model = YOLO(".../football-possession-yolo/models/yolo11_fine_tuned/weights/best.pt")
 
-# Đọc ảnh
+# Path to a directory containing test images
 image_path = ".../football-possession-yolo/data/track-football-player--4/test/images"
+
+# Run inference on all images inside the folder
 results = model.predict(image_path, conf=0.5)
 
-# Lấy ảnh đã vẽ bounding box
+# Select one prediction result (e.g., the 9th image)
 annotated = results[8].plot()
 
-# Hiển thị
+# Display the image with bounding boxes
 cv2.imshow("Result", annotated)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
